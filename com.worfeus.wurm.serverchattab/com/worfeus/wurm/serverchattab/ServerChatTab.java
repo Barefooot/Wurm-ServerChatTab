658453f449c5056dd90e407748940e278788d080
exodev <exopioneer.exodev@gmail.com>
2020-08-08 01:18:21 +0300

    WIP - multi-tabs

--- com.worfeus.wurm.serverchattab/com/worfeus/wurm/serverchattab/ServerChatTab.java
+++ com.worfeus.wurm.serverchattab/com/worfeus/wurm/serverchattab/ServerChatTab.java
@@ -16,42 +16,67 @@ import org.gotti.wurmunlimited.modloader.interfaces.PlayerMessageListener;
 import org.gotti.wurmunlimited.modloader.interfaces.WurmServerMod;
 
 public class ServerChatTab implements WurmServerMod, Configurable, PlayerLoginListener, PlayerMessageListener {
-	private static final Logger logger = Logger.getLogger("ServerChatTab");
-	static String modName = "ServerChatTab"; 
+	private static final String modName = "ServerChatTab"; 
+	private static final Logger logger = Logger.getLogger(modName);
 	static final String EmptyString = "";
 	static final String ColorWhiteString = "FFFFFF";
 	
 
 	
     boolean enabled;
+    
+    int tabsCount;
 
-    String chatTab1Name;
-    String[] chatTab1Lines;
-    Color chatTab1Color = Color.white;
-    Boolean chatTab1Typable;
+    String[] tabsNames;
+    String[][] tabsLines;
+    Color[] tabsColors;
+    Boolean[] tabsTypables;
     
     
     public void configure(Properties properties) {
-    	enabled = Boolean.valueOf(properties.getProperty("enabled", "false"));
+    	try {
+    		enabled = Boolean.valueOf(properties.getProperty("enabled", "false"));
+		} catch (Exception ex) {
+			enabled = false;
+		}
     	if (!enabled) return;
     	
-    	chatTab1Name = String.valueOf(properties.getProperty("chatTab1Name", EmptyString)).trim();
-        chatTab1Lines = String.valueOf(properties.getProperty("chatTab1Lines", EmptyString)).trim().split("[|]{2}");
-        chatTab1Typable = Boolean.valueOf(properties.getProperty("chatTab1Typable", "false").trim());
-        chatTab1Color = Color.decode("#"+properties.getProperty("chatTab1Color", ColorWhiteString).trim());
-    	chatTab1Name = String.valueOf(properties.getProperty("chatTab1Name", EmptyString)).trim();
+    	try {
+    		tabsCount = Math.max(0, Integer.valueOf(properties.getProperty("tabsCount", "0").trim()));
+		} catch (Exception e) {
+    		tabsCount = 0;
+		}
+    	if (tabsCount == 0) return;
+
+	    tabsNames = new String[tabsCount];
+	    tabsLines = new String[tabsCount][];
+	    tabsColors = new Color[tabsCount];
+	    tabsTypables = new Boolean[tabsCount];
+    	
+    	for (int tabIdx = 0; tabIdx < tabsCount; tabIdx++) {
+	    	tabsNames[tabIdx] = String.valueOf(properties.getProperty("tabName."+tabIdx, EmptyString)).trim();
+	        tabsLines[tabIdx] = String.valueOf(properties.getProperty("tabLines."+tabIdx, EmptyString)).trim().split("[|]{2}");
+	        tabsColors[tabIdx] = Color.decode("#"+properties.getProperty("tabColor."+tabIdx, ColorWhiteString).trim());
+	        tabsTypables[tabIdx] = Boolean.valueOf(properties.getProperty("tabTypable."+tabIdx, "false").trim());
+		}
     }
 
     public void onPlayerLogin(Player player){
     	if (!enabled) return;
-    	
-        try {
-        	for(String line : chatTab1Lines)
-            player.getCommunicator().sendMessage(new Message(null, (byte) 0, chatTab1Name, line, chatTab1Color.getRed(), chatTab1Color.getGreen(), chatTab1Color.getBlue()));
-        } catch (Throwable ex) {
-            logSevere("Error loading " + modName + " mod", ex);
-            throw new RuntimeException(ex);
-        }
+
+		for (int tabIdx = 0; tabIdx < tabsCount; tabIdx++) {
+			if (tabsNames[tabIdx] == EmptyString) continue;
+			
+	        try {
+	        	for(String tabLines : tabsLines[tabIdx]) {
+					Color tabColor = tabsColors[tabIdx];
+					player.getCommunicator().sendMessage(new Message(null, (byte) 0, tabsNames[tabIdx], tabLines, tabColor.getRed(), tabColor.getGreen(), tabColor.getBlue()));
+				}
+	        } catch (Throwable ex) {
+	            logWarning(modName + ": error parsing tabIdx=" + tabIdx + ", disabling tab!");
+	            tabsNames[tabIdx] = EmptyString;
+	        }
+		}
     }
 
     @Deprecated
@@ -62,8 +87,13 @@ public class ServerChatTab implements WurmServerMod, Configurable, PlayerLoginLi
 
     @Override
     public MessagePolicy onPlayerMessage(Communicator communicator, String message, String title) {
-//        if (title.equals(chatTab1Name) && !chatTab1Typable && !message.startsWith("#") && !message.startsWith("/"))
-//            return MessagePolicy.DISCARD;
+    	if (!enabled) return MessagePolicy.PASS;
+
+//		for (int tabIdx = 0; tabIdx < tabsCount; tabIdx++) {
+////	        if (title.equals(tab1Name) && !tab1Typable && !message.startsWith("#") && !message.startsWith("/"))
+////	            return MessagePolicy.DISCARD;
+//		}
+    
         return MessagePolicy.PASS;
     }
 
